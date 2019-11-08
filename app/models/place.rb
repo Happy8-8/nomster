5374692005a48c0126091cc8e37cc67da8e8fd57
Alung-Amoh GF <gafonji@gmail.com>
2019-11-08 00:08:17 +0000

    Issues with google API

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ApplicationRecord
-belongs_to :user
-validates :name, presence: true, length: { minimum: 3, too_short: "is too short (minimum %{count} characters)" }
-validates :address, presence: true, length: { in: 17..29 }
-validates :description, presence: true, length: { maximum: 47, too_long: "%{count} characters is the maximum allowed" }
+    belongs_to :user
+    geocoded_by :address
+    after_validation :geocode
+
+    validates :name, presence: true, length: { minimum: 3, too_short: "is too short (minimum %{count} characters)" }
+    validates :address, presence: true, length: { in: 11..59 }
+    validates :description, presence: true, length: { maximum: 97, too_long: "%{count} characters is the maximum allowed" } 
 end
