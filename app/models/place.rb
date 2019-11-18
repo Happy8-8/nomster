50dda28e328c6a0723b9fc2e566f881a94d5bd0e
Alung-Amoh GF <gafonji@gmail.com>
2019-11-18 21:31:18 +0000

    Add image upload feature

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,11 @@
 class Place < ApplicationRecord
     belongs_to :user
     has_many :comments
+    has_many :photos
     geocoded_by :address
     after_validation :geocode
 
     validates :name, presence: true, length: { minimum: 3, too_short: "is too short (minimum %{count} characters)" }
     validates :address, presence: true, length: { in: 11..59 }
-    validates :description, presence: true, length: { maximum: 97, too_long: "%{count} characters is the maximum allowed" } 
+    validates :description, presence: true, length: { maximum: 371, too_long: "%{count} characters is the maximum allowed" } 
 end
