1aaf24b2273f22270300b895080ed3005d81fa62
Alung-Amoh GF <gafonji@gmail.com>
2019-11-18 22:54:22 +0000

    working on issues

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,6 @@ class Place < ApplicationRecord
     after_validation :geocode
 
     validates :name, presence: true, length: { minimum: 3, too_short: "is too short (minimum %{count} characters)" }
-    validates :address, presence: true, length: { in: 11..59 }
+    validates :address, presence: true, length: { in: 11..159 }
     validates :description, presence: true, length: { maximum: 371, too_long: "%{count} characters is the maximum allowed" } 
 end
